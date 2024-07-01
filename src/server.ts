97d97746655d5b726bda2b058a98dfa07557ac07
Deepak973 <deepak.rj.dr@gmail.com>
2024-07-01 17:02:25 +0530

    cors issue solved

--- src/server.ts
+++ src/server.ts
@@ -12,20 +12,42 @@ dotenv.config();
 
 const app = express();
 
-const corsOptions = {
-  origin: '*', // Allow all origins
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
-};
+// const corsOptions = {
+//   origin: '*',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+// };
 
-app.use(cors(corsOptions));
+// app.options('*', cors(corsOptions));
+// app.use(cors(corsOptions));
 
-// Add a pre-flight route for OPTIONS requests
-app.options('*', cors(corsOptions));
+app.use((req, res, next) => {
+  const allowedOrigins = [
+    'https://token-resurrection.vercel.app',
+    'http://localhost:3000',
+  ]; // Add your frontend URLs
+  const origin = req.headers.origin;
 
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+
+  res.removeHeader('x-powered-by');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS',
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
